3fd48e99717642dea2abf7be6f15710ffe3f75a0
Ansel Sermersheim <ansel@copperspice.com>
2019-05-27 08:44:17 -0700

    Remove names for unused parameters in lr_guarded

--- src/libguarded/lr_guarded.hpp
+++ src/libguarded/lr_guarded.hpp
@@ -232,14 +232,14 @@ auto lr_guarded<T, M>::try_lock_shared() const -> shared_handle
 
 template <typename T, typename M>
 template <typename Duration>
-auto lr_guarded<T, M>::try_lock_shared_for(const Duration & duration) const -> shared_handle
+auto lr_guarded<T, M>::try_lock_shared_for(const Duration &) const -> shared_handle
 {
     return lock_shared();
 }
 
 template <typename T, typename M>
 template <typename TimePoint>
-auto lr_guarded<T, M>::try_lock_shared_until(const TimePoint & timepoint) const -> shared_handle
+auto lr_guarded<T, M>::try_lock_shared_until(const TimePoint &) const -> shared_handle
 {
     return lock_shared();
 }
