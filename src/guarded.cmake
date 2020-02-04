b293b2814b6dfe9957bb5fe0040f2724546a7a46
Ansel Sermersheim <ansel@copperspice.com>
2020-02-04 15:34:12 -0800

    Export library for downstream projects which use CMake

--- src/guarded.cmake
+++ src/guarded.cmake
@@ -4,12 +4,13 @@ target_include_directories(
    CsLibGuarded
    INTERFACE
    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>
+   $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
 )
 
 target_compile_features(
    CsLibGuarded
    INTERFACE
-   cxx_std_14
+   cxx_std_17
 )
 
 set(CS_LIBGUARDED_INCLUDE
@@ -23,9 +24,22 @@ set(CS_LIBGUARDED_INCLUDE
    ${CMAKE_CURRENT_SOURCE_DIR}/src/shared_guarded.hpp
 )
 
+
 install(
    FILES ${CS_LIBGUARDED_INCLUDE}
    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-   COMPONENT Devel
+   COMPONENT CsLibGuarded
 )
 
+install(
+   TARGETS CsLibGuarded
+   EXPORT CsLibGuarded-export
+   DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+   COMPONENT CsLibGuarded
+)
+
+install(
+   EXPORT CsLibGuarded-export
+   FILE CsLibGuarded.cmake
+   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/CsLibGuarded
+   )
